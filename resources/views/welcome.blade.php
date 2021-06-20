cae4337e2d73d6b1a292b8c9471d4c230d8e0af3
Iqbal Bin Mohamad Zainuddin <60773534+iqbalzainuddin@users.noreply.github.com>
2021-06-21 02:34:42 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0"> Hi, Welcome! {{ route('login') }} </h1>
+                    <h1 class="m-0"> Hi, Welcome! {{ secure_url(route('login')) }} </h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <!-- <ol class="breadcrumb float-sm-right">
